2258220e54944cbd93431e543fdf47cd797f1344
Khayam Ijaz <ijazkhayam@gmail.com>
2024-12-21 08:54:40 +0500

    Starting Frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Account from "./Pages/Account";
 
 const App = () => {
-  return <div></div>;
+  return <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Account />} />
+      </Routes>
+    </Router>
+  </>;
 };
 
 export default App;
---
+++ Frontend/src/Pages/Account.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Account = () => {
+  return (
+    <>
+          <div className="w-full h-screen bg-zinc-900">
+              
+      </div>
+    </>
+  );
+};
+
+export default Account;
