d2ecadb0be5a32aa76ba3a199f5963d427a606f2
Khayam Ijaz <ijazkhayam@gmail.com>
2024-12-24 15:52:12 +0500

    post backend

--- Backend/Controller/postController.js
+++ Backend/Controller/postController.js
@@ -3,11 +3,12 @@ const Posts = require('../Models/postModel')
 const uploadPost = asyncHandler(async (req, res) => {
     const { caption, content } = req.body;
     try {
-        const newPost = Posts.create({
+        const newPost = await Posts.create({
             user: req.user._id,
             caption,
             content
         });
+        res.send(newPost)
     } catch (error) {
         throw new Error(error)
     }
